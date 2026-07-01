0f7810c7973f71117a9c166ed1d1b96deb85430f
“Adeun” <adeunilemobola@gmail.com>
2026-07-01 11:47:41 -0700

    feat: enhance project setup with dependency management and UI template integration

--- src/sharedTypes.rs
+++ src/sharedTypes.rs
@@ -6,6 +6,94 @@ pub struct TemplateFile {
     pub output_path: String,
 }
 
+pub  struct  CargoDependency {
+    pub name: &'static str,
+    pub version: &'static str,
+     pub features: &'static [&'static str]
+}
+
+pub static TAURI_DEPENDENCY_LIST: [CargoDependency; 5] = [
+    CargoDependency {
+        name: "anyhow",
+        version: "1",
+        features: &[],
+    },
+    CargoDependency{
+        name: "serde",
+        version: "1",
+        features: &["derive"],
+    },
+    CargoDependency{
+        name: "serde_json",
+        version: "1",
+        features: &[],
+    },
+    CargoDependency{
+        name: "serialport",
+        version: "4",
+        features: &[],
+    },
+    CargoDependency{
+        name: "log",
+        version: "0.4",
+        features: &[],
+    },
+];
+pub  struct  UiTemplate {
+    pub name: &'static str,
+    pub source_path: &'static str,
+    pub output_path: &'static str,
+    pub swap:bool,
+    pub replacement: &'static str
+}
+macro_rules! ui_template {
+    ($path:literal) => {
+        UiTemplate {
+            name: $path,
+            source_path: concat!(
+                "https://raw.githubusercontent.com/esp-rust/esp-rust-templates/v0/ui/",
+                $path
+            ),
+            output_path: concat!("/", $path),
+            swap: false,
+            replacement: "",
+        }
+    };
+}
+
+pub static UI_TEMPLATE_LIST: [UiTemplate; 23] = [
+    ui_template!("vite.config.ts"),
+    ui_template!("tsconfig.node.json"),
+    ui_template!("tsconfig.json"),
+    ui_template!("components.json"),
+
+    ui_template!("src/main.tsx"),
+    ui_template!("src/Layout.tsx"),
+    ui_template!("src/App.css"),
+
+    ui_template!("src/components/LogFrame.tsx"),
+    ui_template!("src/components/theme-provider.tsx"),
+
+    ui_template!("src/Hook/state.ts"),
+    ui_template!("src/Hook/Zod.ts"),
+
+    ui_template!("src/lib/logging.ts"),
+    ui_template!("src/lib/utils.ts"),
+
+    ui_template!("src/page/App.tsx"),
+    ui_template!("src/page/Dashboard.tsx"),
+    ui_template!("src/page/Devices.tsx"),
+    ui_template!("src/page/Logs.tsx"),
+    ui_template!("src/page/NotFound.tsx"),
+    ui_template!("src/page/PortSettings.tsx"),
+
+    // TAURI project files
+    ui_template!("src-tauri/src/lib.rs"),
+    ui_template!("src-tauri/src/shared_types/mod.rs"),
+    ui_template!("src-tauri/src/shared_types/event.rs"),
+    ui_template!("src-tauri/src/shared_types/command.rs"),
+];
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GitHubItem {
     pub name: String,
--- src/utility.rs
+++ src/utility.rs
@@ -1,4 +1,7 @@
-use crate::sharedTypes::{LogType, ProgressEvent, ProgressLogShape, ProgressType, UIReplacement, UISourceFile};
+use crate::sharedTypes::{
+    CargoDependency, LogType, ProgressEvent, ProgressLogShape, ProgressType, TAURI_DEPENDENCY_LIST,
+    UI_TEMPLATE_LIST, UIReplacement, UISourceFile,
+};
 use anyhow::Result;
 use std::io::{self, Write};
 use std::path::Path;
@@ -136,26 +139,120 @@ pub fn progress_log(stage: ProgressType, message: String, id: String) {
     println!("__ESP_PROGRESS__:{}", json);
 }
 
-pub fn pre_build_ui(project_path: &Path) -> bool {
+async fn  add_dependency(project_path: &str, dep: &CargoDependency, id: &str) -> std::io::Result<()> {
+    progress_log(
+        ProgressType::Installing,
+        format!("Adding dependency '{}'...", dep.name),
+        id.to_string(),
+    );
+
+    let package = format!("{}@{}", dep.name, dep.version);
+
+    let mut command = Command::new("cargo");
+
+    command.current_dir(project_path).arg("add").arg(package);
+
+    for feature in dep.features {
+        command.arg("--features").arg(feature);
+    }
+
+    let status = command.status()?;
+    if status.success() {
+        progress_log(
+            ProgressType::Complete,
+            format!("Dependency '{}' added.", dep.name),
+            id.to_string(),
+        );
+        Ok(())
+    } else {
+        progress_log(
+            ProgressType::Error,
+            format!("Failed to add dependency '{}'.", dep.name),
+            id.to_string(),
+        );
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!(
+                "cargo add failed for '{}' (exit status: {})",
+                dep.name, status
+            ),
+        ))
+    }
+}
+
+pub async fn pre_build_ui(project_path: &Path) -> bool {
     progress_log(
         ProgressType::Installing,
         "Installing Tauri app with bun...".to_string(),
         "pre-build-ui".to_string(),
     );
 
-    let status = Command::new("bun")
-        .args(["create", "tauri-app"])
-        .current_dir(project_path)
-        .status();
+    let ui_project_name = "ui";
 
+    let status = tokio::process::Command::new("bun")
+        .args(["create", "tauri-app", ui_project_name, "-y"])
+        .current_dir(project_path)
+        .status()
+        .await;
     match status {
         Ok(s) if s.success() => {
+            let mut has_failed = false;
             progress_log(
                 ProgressType::Complete,
                 "Tauri app created successfully.".to_string(),
                 "pre-build-ui".to_string(),
             );
-            true
+
+            let ui_project_path = project_path.join(ui_project_name);
+            let src_tauri_path = ui_project_path.join("src-tauri");
+            let src_tauri_path = src_tauri_path.to_str().unwrap_or_default();
+
+            progress_log(
+                ProgressType::Installing,
+                format!("Installing {} dependencies...", TAURI_DEPENDENCY_LIST.len()),
+                "pre-build-ui".to_string(),
+            );
+
+            for dep in TAURI_DEPENDENCY_LIST.iter() {
+                if let Err(e) = add_dependency(src_tauri_path, dep, "pre-build-ui").await {
+                    progress_log(
+                        ProgressType::Error,
+                        format!("Failed to run cargo add for '{}': {}", dep.name, e),
+                        "pre-build-ui".to_string(),
+                    );
+                }
+            }
+            for file_ob in UI_TEMPLATE_LIST.iter() {
+                let output_path = ui_project_path.join(file_ob.output_path.trim_start_matches('/'));
+                let _ = download_file(&file_ob.source_path, &output_path).await;
+
+                match tokio::fs::try_exists(&output_path).await {
+                    Ok(true) => {
+                        progress_log(
+                            ProgressType::Complete,
+                            format!("Downloaded template file '{}'.", file_ob.name),
+                            "pre-build-ui".to_string(),
+                        );
+                    }
+                    _ => {
+                        progress_log(
+                            ProgressType::Error,
+                            format!("Template file '{}' was not created.", file_ob.name),
+                            "pre-build-ui".to_string(),
+                        );
+                        has_failed = true;
+                        break;
+                    }
+                }
+            }
+
+            progress_log(
+                ProgressType::Finished,
+                "Tauri dependency installation complete.".to_string(),
+                "pre-build-ui".to_string(),
+            );
+
+            if has_failed { false } else { true }
         }
         Ok(s) => {
             log(
@@ -174,9 +271,4 @@ pub fn pre_build_ui(project_path: &Path) -> bool {
             false
         }
     }
-}
-
-pub async fn build_ui(_files: Vec<UISourceFile>, _replacements: Vec<UIReplacement>) {
-    // TODO: download files from source_url to output_path
-    // TODO: apply replacements from source_url into target_path
 }
